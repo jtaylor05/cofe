00e524d88f386d638f8a354fcc586e11f6812ad2
Jackson Taylor <jacksontay666@gmail.com>
2026-09-06 19:01:05 -0400

    added in configuration options for module

--- src/cofe/__main__.py
+++ src/cofe/__main__.py
@@ -1,10 +1,12 @@
 import sys
 
+import random
+
 import argparse as ap
 
 from . import PythonLauncher, PackageConfigParser, install_import_hook
 
-def config_command(raw_changes: list[str]):
+def add_changes(raw_changes: list[str]):
     print("changes:", raw_changes)
     changes = {d[0]:d[1] for d in [c.split('=') for c in raw_changes]}
     
@@ -13,6 +15,48 @@ def config_command(raw_changes: list[str]):
         config.set_default(k, v)
     
     config.write()
+    
+def add_transformers(raw_transformers: list[str]):
+    print("transformers: ", raw_transformers)
+    transformers = {d[0]:d[1] for d in [t.split('=') for t in raw_transformers]}
+    
+    config = PackageConfigParser()
+    for k, v in transformers.items():
+        config.add_transformer(k, v)
+        
+    config.write()
+    
+def add_active(new_active: list[str]):
+    print("New active: ", new_active)
+    
+    config = PackageConfigParser()
+    for a in new_active:
+        config.add_active(a)
+    
+    config.write()
+    
+def remove_transformers(old_transformers: list[str]):
+    config = PackageConfigParser()
+    for t in old_transformers:
+        config.remove_transformer(t)
+    config.write()
+    
+def remove_active(old_active: list[str]):
+    config = PackageConfigParser()
+    for a in old_active:
+        config.remove_active(a)
+    config.write()
+
+def randomize_active(n: int):
+    config = PackageConfigParser()
+    
+    config.clear_active()
+    
+    selection = random.sample(config.get_available(), n)
+    
+    for s in selection:
+        config.add_active(s)
+    
 
 def exec_command(file_path: str, remainder: list[str]):
     sys.argv = [file_path] + remainder
@@ -32,8 +76,13 @@ if __name__ == "__main__":
     subparsers = parser.add_subparsers(dest="command", required=True, help="Which submodule to use.")
     
     config_parser = subparsers.add_parser("config", help="configure interpreter settings and modules.")
-    config_parser.add_argument("-c", "--change", nargs='*', type=str, help="Changes made to current configuration.")
+    config_parser.add_argument("-c", "--change", nargs='*', type=str, help="Changes made to current configuration (key=value).")
     config_parser.add_argument("-r", "--restore", action='store_true', help="Restores defaults on the current configuration.")
+    config_parser.add_argument("-t", "--transformer", nargs='*', type=str, help="Add new transformer class, as well as file (cls=file).")
+    config_parser.add_argument("-a", "--active", nargs='*', type=str, help="Add new active transformer class.")
+    config_parser.add_argument("-v", "--remove-available", nargs='*', type=str, help="Remove transformer class from available.")
+    config_parser.add_argument("-l", "--remove-active", nargs='*', type=str, help="Remove transformer class from active.")
+    config_parser.add_argument("-R", "--random", type=int, default=1, help="Randomize active transformers.")
     
     exec_parser = subparsers.add_parser("exec", help="acts as an entry point into module code.")
     exec_parser.add_argument("file_path", type=str)
@@ -45,7 +94,17 @@ if __name__ == "__main__":
             if args.restore:
                 PackageConfigParser().restore()
             if args.change:
-                config_command(args.change)
+                add_changes(args.change)
+            if args.transformer:
+                add_transformers(args.transformer)
+            if args.active:
+                add_active(args.active)
+            if args.remove_available:
+                remove_transformers(args.remove_available)
+            if args.remove_active:
+                remove_active(args.remove_active)
+            if args.random:
+                randomize_active(args.random)
             
         case "exec":
             exec_command(args.file_path, remainder)
--- src/cofe/configure.py
+++ src/cofe/configure.py
@@ -1,11 +1,17 @@
 from configparser import ConfigParser
 from pathlib import Path
 
+import ast
+
+from .grammar_transform import GrammarWrapper
+
 MODULE_DIR = ".cenv"
 
 CONFIG_FILENAME = "config.ini"
 
 EXTENSION = "extension"
+
+type Pathlike = str | Path
 
 class InvalidConfigError(Exception):
     """Raised when a config value does not exist in the configuration."""
@@ -19,6 +25,11 @@ class PackageConfigParser(ConfigParser):
             init_config_settings(self.config_file)
         
         self.read(self.config_file)
+        
+        if not self.has_section("available"):
+            self.add_section("available")
+        if not self.has_section("active"):
+            self.add_section("active")
     
     @property
     def extension(self):
@@ -31,6 +42,36 @@ class PackageConfigParser(ConfigParser):
             self['DEFAULT'][key] = val
         else:
             raise ValueError(f"Key {key} is not contained in 'DEFAULT'.")
+        
+    def add_transformer(self, cls_name: str, cls_file: Pathlike):
+        self.set('available', cls_name, str(Path(cls_file)))
+        
+    def remove_transformer(self, cls_name: str):
+        self.remove_option('available', cls_name)
+        
+    def get_available(self) -> list[str]:
+        ret = set()
+        for cls_name in self['available']:
+            ret.add(self['available'][cls_name])
+        return list(ret)
+        
+    def add_active(self, cls_name: str):
+        if cls_name not in self['available']:
+            raise InvalidConfigError(f"{cls_name} not one of the available transformers.")
+        self.set('active', cls_name, self['available'][cls_name])
+        
+    def remove_active(self, cls_name: str):
+        self.remove_option('active', cls_name)
+        
+    def clear_active(self):
+        for cls_name in self['active']:
+            self.remove_active(cls_name)
+
+    def get_active(self) -> list[str]:
+        ret = set()
+        for cls_name in self['active']:
+            ret.add(self['active'][cls_name])
+        return list(ret)
     
     def write(self):
         with open(self.config_file, 'w') as cf:
@@ -45,5 +86,57 @@ def init_config_settings(file: Path):
     
     parser = ConfigParser()
     parser['DEFAULT'] = { EXTENSION : '.y' }
+    parser['available'] = {}
+    parser['active'] = {}
     with open(file, 'w') as cf:
         parser.write(cf)
+        
+class Transform:
+    
+    TRANSFORM_REGISTRY = {}
+    
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls.TRANSFORM_REGISTRY[cls.__name__] = cls
+        
+    def apply_grammar(self, grammar : GrammarWrapper):
+        pass
+    
+    def apply_ast(self, root : ast.AST):
+        pass
+        
+    def get_sort(self):
+        return 0
+        
+    def __lt__(self, other):
+        if not isinstance(other, Transform):
+            raise NotImplementedError("Can only compare with other Transform subclasses.")
+        return self.get_sort() < other.get_sort()
+    
+    def __le__(self, other):
+        if not isinstance(other, Transform):
+            raise NotImplementedError("Can only compare with other Transform subclasses.")
+        return self.get_sort() <= other.get_sort()
+    
+    def __eq__(self, other):
+        if not isinstance(other, Transform):
+            raise NotImplementedError("Can only compare with other Transform subclasses.")
+        return self.get_sort() == other.get_sort()
+    
+    def __ne__(self, other):
+        if not isinstance(other, Transform):
+            raise NotImplementedError("Can only compare with other Transform subclasses.")
+        return self.get_sort() != other.get_sort()
+    
+    def __ge__(self, other):
+        if not isinstance(other, Transform):
+            raise NotImplementedError("Can only compare with other Transform subclasses.")
+        return self.get_sort() >= other.get_sort()
+    
+    def __gt__(self, other):
+        if not isinstance(other, Transform):
+            raise NotImplementedError("Can only compare with other Transform subclasses.")
+        return self.get_sort() > other.get_sort()
+    
+    def __hash__(self):
+        return object.__hash__(self)
